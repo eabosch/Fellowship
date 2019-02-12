3d090c74cb61412347657f110135ab1e0dece77f
eabosch <46727438+eabosch@users.noreply.github.com>
2019-02-12 12:47:43 -0600

    level control, looping background

--- Idyll/Assets/scripts/LevelControl.cs
+++ Idyll/Assets/scripts/LevelControl.cs
@@ -6,23 +6,35 @@ public class LevelControl : MonoBehaviour
 {
 	public string levelname;
 
+    private bool nextLevel;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
+        nextLevel = false;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+        if(Input.GetKeyDown (KeyCode.Space) && nextLevel == true)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(levelname);
+        }
 	}
 
-	/*void OnTriggerEnter(Collider2D other)
-	{
-		if (other.CompareTag ("Player")) 
+	void OnTriggerEnter2D(Collider2D other)
+    { 
+
+        if (other.CompareTag ("Player"))
 		{
-			UnityEngine.SceneManagement.SceneManager.LoadScene(levelname);
-		}
-	}*/
+            nextLevel = true;
+            Debug.Log("Going UP!");
+        }
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        nextLevel = false;
+    }
 }
